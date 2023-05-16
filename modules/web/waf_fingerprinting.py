4e34bbad33c56df7e141d644fde1878a40a1e109
Samsterr <samteo19@gmail.com>
2023-05-16 22:27:25 +0800

    Added Web Tech Analysing and WAF detection

---
+++ modules/web/waf_fingerprinting.py
@@ -0,0 +1,13 @@
+import os
+
+def waf_fingerprint(domain):
+    """Uses Wafw00f to detect prescence of a WAF"""
+    command = "wafw00f -a " + domain + "| grep 'is behind'"
+    output = os.popen(command).read()
+    if output:
+        print("WAF Found\n")
+        print(output)
+    else:
+        print("No WAF detected or Undetectable by wafw00f")
+    
+waf_fingerprint("inlanefreight.com")
---
+++ modules/web/webtechnology_id.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+def web_tech_id(domain):
+    """Identifies the technologies used on a webpage using whatweb and exports it in json format"""
+    command = "whatweb -a3 " + domain + " --log-json=./technologies.txt"
+    os.system(command)
+    with open("technologies.txt", 'r') as f:
+        output = json.load(f)
+    for entries in output:
+        if entries["http_status"] == 200:
+            unparsed_results = entries
+    
+    with open("technologies.json", 'w') as f:
+        f.write(json.dumps(unparsed_results['plugins']))
+        
+
+web_tech_id("inlanefreight.com")
