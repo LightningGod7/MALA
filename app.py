f2f43fa66278b3e7c7580c277011cde657b86613
LightningGod <78635998+LightningGod7@users.noreply.github.com>
2023-06-22 17:43:54 +0800

    Removing dynamic loading
    Some issues with partial importing of a module to get global variables

--- app.py
+++ app.py
@@ -7,42 +7,16 @@ import importlib.util #Dynamically load modules
 config_file_path = r".\configs\universal-configs.json"
 modules_folder_path = r".\modules"
 
-# #Load all modules into dictionary
-# def module_load(modules_folder):
-#     modules = []
-
-#     #Search through module directory for all python files (modules)
-#     for root, dirs, files in os.walk(modules_folder):
-#         for file in files:
-#             if file.endswith(".py"):
-#                 module_path = os.path.join(root, file)
-#                 modules.append(module_path)
-#     return modules
-
+#Load all modules into dictionary
 def module_load(modules_folder):
-    modules = {}
+    modules = []
 
-    # Search through module directory for all python files (modules)
+    #Search through module directory for all python files (modules)
     for root, dirs, files in os.walk(modules_folder):
         for file in files:
             if file.endswith(".py"):
                 module_path = os.path.join(root, file)
-                module_name = os.path.splitext(file)[0]
-
-                # Load the module dynamically
-                spec = importlib.util.spec_from_file_location(module_name, module_path)
-                module = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(module)
-
-                # Retrieve module metadata
-                module_data = {
-                    'name': module.__name__,
-                    'description': module.__description__
-                }
-
-                # Add module to the dictionary
-                modules[module_name] = module_data
-
+                modules.append(module_path)
     return modules
 
 #Load default configs
@@ -50,7 +24,6 @@ def initialize_defaults(config_file):
     with open(config_file) as default_configs:
         universal_variables = json.load(default_configs)
     return universal_variables
-
 
 if __name__ == "__main__":
     modules = module_load(modules_folder_path)
