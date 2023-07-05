e96b5c9b93f8b7a8dfe033eb0571342dc44619a4
Samsterr <samteo19@gmail.com>
2023-07-05 16:19:16 +0800

    waf fingerprinting and ssl module update

--- modules/enumeration/ssl_subdomain.py
+++ modules/enumeration/ssl_subdomain.py
@@ -1,31 +1,52 @@
-import requests
-import json
+class ssl_subdomain:
+    def __init__(self, tools, variables):
 
-__name__ = "test scanner"
-__description__ = "test"
+        ### GET common variables
+        self.variables = variables
+        common_vars = variables.get("common_variables")
+        self.target = common_vars["RHOST"]["Value"]
+        self.port = common_vars["RPORT"]["Value"]
+        self.curl = tools.get("curl")
 
+    def test(self):
+        print("Imported this module")
+        print(self.target)
+        print(self.port)
+        print(self.curl)
+        print("RHOST format is: www.example.com")
 
-def ssl_domain_enum(domain):
-    """This module gets various subdomains listed in a domains SSL cert"""
-    domain_list = []
-    response = requests.get("https://crt.sh/?q=" + domain +"&output=json").json()
-    for entries in response:
-        common_name = entries["common_name"]
-        name_value = entries["name_value"]
+    def get_command_list(self):
+        return self.ssl_domain_enum()
 
-        if common_name not in domain_list:
-            domain_list.append(common_name)
+    def ssl_domain_enum(self):
+        """This module gets various subdomains listed in a domains SSL cert"""
+        if not self.target:
+            print("Not all compulsory options are set. Check with `options` command")
+            return 
         
-        if "\n" in name_value:
-            domains = name_value.split("\n")
-            for domain in domains:
-                if domain not in domain_list:
-                    domain_list.append(domain)
-        else:
-            if name_value not in domain_list:
-                domain_list.append(domain)
-    
-    domain_list.sort()
-    print(domain_list) #remove this
-    
-ssl_domain_enum("inlanefreight.com")
+        prefix = self.curl # add Output
+        target_arg = "https://crt.sh/?q=" + self.target + "&output=json"
+        command_list = [prefix, target_arg]
+        return command_list
+
+
+    #for entries in response:
+    #    common_name = entries["common_name"]
+    #    name_value = entries["name_value"]
+#
+    #    if common_name not in domain_list:
+    #        domain_list.append(common_name)
+    #    
+    #    if "\n" in name_value:
+    #        domains = name_value.split("\n")
+    #        for domain in domains:
+    #            if domain not in domain_list:
+    #                domain_list.append(domain)
+    #    else:
+    #        if name_value not in domain_list:
+    #            domain_list.append(domain)
+    #
+    #domain_list.sort()
+    #print(domain_list) #remove this
+    #
+#
--- modules/enumeration/waf_fingerprinting.py
+++ modules/enumeration/waf_fingerprinting.py
@@ -1,17 +1,33 @@
-import os
+class waf_fingerprint:
+    def __init__(self, tools, variables):
 
-__name__ = "test scanner"
-__description__ = "test"
+        ### Get Common Variables
+        self.variables = variables
+        common_vars = variables.get("common_variables")
+        self.target = common_vars["RHOST"]["Value"]
+        self.port = common_vars["RPORT"]["Value"]
+        self.wafw00f = tools.get("wafw00f")
 
+        # module_variables["output"]
+        self.url = "http://" + self.target
+        if self.port:
+            self.url += ":" + str(self.port)
 
-def waf_fingerprint(domain):
-    """Uses Wafw00f to detect prescence of a WAF"""
-    command = "wafw00f -a " + domain + "| grep 'is behind'"
-    output = os.popen(command).read()
-    if output:
-        print("WAF Found\n")
-        print(output)
-    else:
-        print("No WAF detected or Undetectable by wafw00f")
-    
-waf_fingerprint("inlanefreight.com")
+    def test(self):
+        print("Imported this module")
+        print(self.target)
+        print(self.port)
+        print(self.wafw00f)
+
+    def get_command_list(self):
+        return self.waf_fingerprint()
+
+    def waf_fingerprint(self):
+        if not self.target:
+            print("Not all compulsory options are set. Check with `options` command")
+            return
+        
+        prefix = self.wafw00f
+        target_arg = "-a " + self.url
+        command_list = [prefix, target_arg]
+        return command_list
